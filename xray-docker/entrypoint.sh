e38dbcc6140a427d33e91c84e97c7e060acc527e
caojiaxia <104258329+caojiaxia@users.noreply.github.com>
2026-03-02 23:09:25 +0800

    Update entrypoint.sh

--- xray-docker/entrypoint.sh
+++ xray-docker/entrypoint.sh
@@ -1,8 +1,15 @@
-#!/bin/bash
-export UUID=${UUID:-$(cat /proc/sys/kernel/random/uuid)}
-export XPATH=${XPATH:-/randompath}
+#!/bin/sh
 
-# 这一步会将 template 转换为真正的 config.json
-envsubst '${UUID},${XPATH}' < /etc/xray/config.template.json > /etc/xray/config.json
+# 检查变量是否存在，如果不存在则使用默认值
+UUID=${UUID:-"c67e108d-b135-4acd-b0b4-33f2d18dff44"}
+XPATH=${XPATH:-"/GEdhhrQEkzaq"}
 
-exec /usr/bin/xray -config /etc/xray/config.json
+echo "正在启动 Xray，使用 UUID: $UUID 和 路径: $XPATH"
+
+# 使用 sed 替换模板中的占位符，生成实际的 config.json
+sed -e "s/\$UUID/$UUID/g" \
+    -e "s|\$XPATH|$XPATH|g" \
+    /etc/xray/config.template.json > /etc/xray/config.json
+
+# 启动 xray
+exec xray -config /etc/xray/config.json
